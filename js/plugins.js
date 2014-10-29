7a16d9dc3bd28a3c6756bb671b823e91256fbb82
Steffen Meyer <steffen.meyer@t8y.com>
2014-10-29 08:52:53 +0100

    TICKET-1234 - usefull commit message

--- js/plugins.js
+++ js/plugins.js
@@ -11,7 +11,7 @@
     var length = methods.length;
     var console = (window.console = window.console || {});
 
-    var foo = 'hello world';
+    var foo = 'hallo welt';
     var bar = foo
     console.log(bar);
 
